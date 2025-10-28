010602c7fbd68fdf770db0c73f8e32ff59e132fd
Nikolas, Vardeberg <nikolas.vardeberg1@gmail.com>
2025-10-28 17:37:41 +0100

    feat: not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyTitle,
+} from "@/components/ui/empty";
+
+export default function NotFound() {
+  return (
+    <Empty className="h-screen">
+      <EmptyHeader>
+        <EmptyTitle>404 - Not Found</EmptyTitle>
+        <EmptyDescription>
+          The page you&apos;re looking for doesn&apos;t exist. Try searching for
+          what you need below.
+        </EmptyDescription>
+      </EmptyHeader>
+    </Empty>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import { ToggleTheme } from "@/components/toggle-theme";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
-  return <ToggleTheme />;
+  return (
+    <div className="max-w-[800px] mx-auto flex py-5 flex-col gap-3">
+      <Skeleton className="w-full h-14" />
+
+      <Skeleton className="w-full h-96" />
+      <Skeleton className="w-full h-96" />
+      <Skeleton className="w-full h-96" />
+    </div>
+  );
 }
