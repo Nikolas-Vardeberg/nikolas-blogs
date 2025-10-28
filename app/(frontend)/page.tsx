89ba23a35c9fbeecc4223b5eb94ae9e98272ca78
Nikolas, Vardeberg <nikolas.vardeberg1@gmail.com>
2025-10-28 20:36:41 +0100

    feat: tiptap rich text editor

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,12 +1,11 @@
-import { ToggleTheme } from "@/components/toggle-theme";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
   return (
     <div className="max-w-[800px] mx-auto flex py-5 flex-col gap-3">
       <Skeleton className="w-full h-14" />
-
       <Skeleton className="w-full h-96" />
+
       <Skeleton className="w-full h-96" />
       <Skeleton className="w-full h-96" />
     </div>
